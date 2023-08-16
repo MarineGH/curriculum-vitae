85b3d188c8489bddfdc0590818eeb4440ec10af4
Alexandre Bouvier <alexandre.b2506@gmail.com>
2023-08-16 21:05:32 +0200

    debug CI

--- texlive_install.sh
+++ texlive_install.sh
@@ -11,60 +11,60 @@
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 # rm -r /tmp/texlive/*
 # rm -r ${HOME}/.texlive/* # vide le cache
-if ! command -v texlua > /dev/null; then
-  # Obtain TeX Live
-  wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-  tar -xzf install-tl-unx.tar.gz
-  cd install-tl-20*
+# if ! command -v texlua > /dev/null; then
+#   # Obtain TeX Live
+#   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+#   tar -xzf install-tl-unx.tar.gz
+#   cd install-tl-20*
 
-  # Install a minimal system
-  ./install-tl --profile=../texlive/texlive.profile
+#   # Install a minimal system
+#   ./install-tl --profile=../texlive/texlive.profile
 
-  cd ..
-fi
+#   cd ..
+# fi
 
 # Just including texlua so the cache check above works
 # Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
+# tlmgr install luatex
 
-tlmgr search --global --file tikz.sty
+# tlmgr search --global --file tikz.sty
 
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
-tlmgr install   \
-  ec            \
-  exam          \
-  etoolbox      \
-  l3packages    \
-  geometry      \
-  xcolor        \
-  moderncv      \
-  amsfonts      \
-  stmaryrd      \
-  l3kernel      \
-  microtype     \
-  pgf           \
-  cm-super      \
-  pdftexcmds    \
-  tlmgr         \
-  kvoptions     \
-  infwarerr     \
-  letltxmacro   \
-  epstopdf-base \
-  ltxcmds       \
-  kvsetkeys     \
-  kvdefinekeys  \
-  pdfescape     \
-  xcolor        \
-  hycolor       \
-  auxhook       \
-  intcalc       \
-  etexcmds      \
-  amsmath       \
-  scheme-full
+# tlmgr install   \
+#   ec            \
+#   exam          \
+#   etoolbox      \
+#   l3packages    \
+#   geometry      \
+#   xcolor        \
+#   moderncv      \
+#   amsfonts      \
+#   stmaryrd      \
+#   l3kernel      \
+#   microtype     \
+#   pgf           \
+#   cm-super      \
+#   pdftexcmds    \
+#   tlmgr         \
+#   kvoptions     \
+#   infwarerr     \
+#   letltxmacro   \
+#   epstopdf-base \
+#   ltxcmds       \
+#   kvsetkeys     \
+#   kvdefinekeys  \
+#   pdfescape     \
+#   xcolor        \
+#   hycolor       \
+#   auxhook       \
+#   intcalc       \
+#   etexcmds      \
+#   amsmath       \
+#   scheme-full
 
 # Keep no backups (not required, simply makes cache bigger)
-tlmgr option -- autobackup 0
+# tlmgr option -- autobackup 0
 
 # Update the TL install but add nothing new
-tlmgr update --self --all --no-auto-install
+# tlmgr update --self --all --no-auto-install
